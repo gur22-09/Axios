a42decdb540586ea4974b76683aaf3bee647fb15
GURPRIT <tirprug@users.noreply.github.com>
2020-05-08 13:32:23 +0530

    initial commit

--- start.js
+++ start.js
@@ -1,26 +1,67 @@
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  // axios({
+  //   method:'get',
+  //   url:'https://jsonplaceholder.typicode.com/todos',
+  //   params:{
+  //     _limit:5
+  //   }
+  // }).then(res=>showOutput(res))
+  // .catch(err=>console.error(err));
+
+  //the above way is a long way, there is a more practical way to do it
+  //either you can use axios.get or by default if nothing is specified it is considered a get request
+
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title:'New todo',
+    completed:false
+  }).then(res=>showOutput(res))
+  .catch(err=>console.error(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  //every api works differently here we had to specify in the endpoint which id todo we wanted to put/post
+  //in our case we have used 2
+  axios.patch('https://jsonplaceholder.typicode.com/todos/2',{
+    title:'work todo',
+    completed:false
+  }).then(res=>showOutput(res))
+  .catch(err=>console.error(err));
+
+  // axios.put('https://jsonplaceholder.typicode.com/todos/2',{
+  //   title:'work todo',
+  //   completed:false
+  // }).then(res=>showOutput(res))
+  // .catch(err=>console.error(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios.delete('https://jsonplaceholder.typicode.com/todos/2')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  //simultaneous request means using both get or put request at the same time..you will se
+
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
+    axios.get('https://jsonplaceholder.typicode.com/comments?_limit=5')
+  ]).then(axios.spread((todos,comments)=>{
+    console.log(todos);
+    console.log(comments);
+  }))
+  .catch(err=>console.error(err));
 }
 
 // CUSTOM HEADERS
